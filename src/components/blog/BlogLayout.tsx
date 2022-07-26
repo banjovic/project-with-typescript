f3fa160a3c196e0182c70aa26f50c5139185478a
Edgar John <45301854+ebinabo@users.noreply.github.com>
2022-07-26 13:25:19 +0300

    Update BlogLayout.tsx

--- src/components/blog/BlogLayout.tsx
+++ src/components/blog/BlogLayout.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
 
-const BlogLayout: FC = () => {
-  return <div>BlogLayout</div>;
+const BlogLayout: FC = (props) => {
+  const {children} = props
+  return <div>{children}</div>;
 };
 
 export default BlogLayout;
